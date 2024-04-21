44851b7dd2241a6366b92c12d6e80628d221d853
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-21 21:08:55 +0300

    Add title and askew nav to header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,46 @@
 <template>
     <div>
+        <header>
+            <div class="container">
+                <span class="title">masters bookstore</span>
+                <nav>
+                    книги
+                    искусство
+                    аксессуары
+                    контакты
+                </nav>
+            </div>
+        </header>
         <slot />
     </div>
 </template>
+
+<style scoped lang="scss">
+header {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: white;
+
+    .container {
+        max-width: 1206px;
+        flex-grow: 1;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 25px;
+
+        .title {
+            font-size: 24px;
+            font-weight: 600;
+            padding: 3px;
+            color: $primary;
+            background-color: $primary-background;
+        }
+
+        nav {
+            font-size: 18px;
+        }
+    }
+}
+</style>
