76e4b8eaebd25b7346486d9fc9dc7581254bcfa6
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-21 16:08:03 +0300

    Configure app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+    devtools: { enabled: true },
 
-  typescript: { strict: true },
+    typescript: { strict: true },
 
-  modules: [
-    '@pinia/nuxt',
-    '@nuxt/test-utils/module',
-  ],
-})
+    modules: [
+        '@pinia/nuxt', 
+        '@nuxt/test-utils/module'
+    ],
 
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        },
+    },
+
+    alias: {
+        '@': '/<srcDir>',
+        '@@': '/<rootDir>',
+        assets: '/srcDir/assets',
+    },
+});
