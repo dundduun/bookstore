a26d33bc868fb101916f7a4e36d3edb364d3570a
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-21 21:07:24 +0300

    Apply fonts to app

--- app.vue
+++ app.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
+import '@/assets/styles/gilroy-font.scss'
+
 useHead({
     title: 'Masters Bookstore - книжный магазин',
     meta: [
         { name: 'author', content: 'dundduun' },
-        {
-            name: 'description',
-            content:
-                'Книжный магазин редких и эксклюзивных изданий по истории искусства, архитектуре, дизайну, моде и фотографии.',
-        },
+        { name: 'description', content: 'Книжный магазин редких и эксклюзивных изданий по истории искусства, архитектуре, дизайну, моде и фотографии.', },
     ],
     link: [
-        {
-            rel: 'icon',
-            type: 'image/x-icon',
-            href: '/favicon.ico',
-        },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', },
     ],
 });
 </script>
 
 <template>
-    <NuxtLayout> </NuxtLayout>
+    <div class="body">
+        <NuxtLayout> </NuxtLayout>
+    </div>
 </template>
+
+<style lang="scss">
+body {
+    margin: 0;
+}
+
+div.body {
+    font-family: Gilroy;
+    font-weight: 300;
+}
+</style>
