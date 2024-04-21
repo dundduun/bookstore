4fc6067dde5b5e4fdc67c7957fabd3069f2ba364
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-21 16:00:42 +0300

    Add head to app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+useHead({
+    title: 'Masters Bookstore - книжный магазин',
+    meta: [
+        { name: 'author', content: 'dundduun' },
+        {
+            name: 'description',
+            content:
+                'Книжный магазин редких и эксклюзивных изданий по истории искусства, архитектуре, дизайну, моде и фотографии.',
+        },
+    ],
+    link: [
+        {
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico',
+        },
+    ],
+});
+</script>
+
 <template>
-  <div>
-    
-  </div>
+    <NuxtLayout> </NuxtLayout>
 </template>
