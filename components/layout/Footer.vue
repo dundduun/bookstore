22e170a1d5d94ed943ddc2517423c82f2eebb757
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-23 17:32:57 +0300

    Decompose to Header and Footer
    At footer make cascade to masters project links

---
+++ components/layout/Footer.vue
@@ -0,0 +1,113 @@
+<template>
+    <footer>
+        <div class="links-container">
+            <div class="masters-project">
+                <div class="name-and-media">
+                    <div class="masters-project-name">
+                        masters<br />
+                        project
+                    </div>
+
+                    <div class="media">
+                        <a href="https://t.me/artschoolmasters">
+                            <img
+                                class="img-media-link"
+                                src="@/assets/images/black-telegram.png"
+                            />
+                        </a>
+
+                        <a href="https://vk.com/artschoolmaste">
+                            <img
+                                class="img-media-link"
+                                src="@/assets/images/black-vk.png"
+                            />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="school-and-subscribe-links">
+                    <a href="https://masters-project.ru/">
+                        школа<br />
+                        masters
+                    </a>
+
+                    <a href="https://masters-project.ru/subscriptions-promo/">
+                        подписка<br />
+                        masters
+                    </a>
+                </div>
+            </div>
+
+            <div class="masters-bookstore"></div>
+        </div>
+
+        <div class="news"></div>
+    </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+    margin-top: 100px;
+    border: 1px black solid;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+
+    .links-container {
+        max-width: 1206px;
+        flex-grow: 1;
+        display: flex;
+        align-items: center;
+
+        .masters-project {
+            width: 400px;
+            height: 161px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            border: 1px black solid;
+
+            .name-and-media {
+                display: flex;
+                justify-content: space-between;
+
+                .masters-project-name {
+                    font-size: 40px;
+                }
+
+                .media {
+                    display: flex;
+                    gap: 6px;
+
+                    .img-media-link {
+                        width: 27px;
+                        height: 27px;
+                    }
+                }
+            }
+
+            .school-and-subscribe-links {
+                display: flex;
+                justify-content: space-between;
+
+                a {
+                    text-decoration: none;
+                    color: inherit;
+                }
+            }
+        }
+
+        .masters-bookstore {
+            width: 700px;
+            height: 161px;
+            border: 1px red solid;
+        }
+    }
+
+    .news {
+        width: 100%;
+    }
+}
+</style>
---
+++ components/layout/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <header>
+        <div class="container">
+            <span class="site-title">masters bookstore</span>
+            <nav>
+                <NuxtLink class="nuxt-link">книги</NuxtLink>
+                <NuxtLink class="nuxt-link">искусство</NuxtLink>
+                <NuxtLink class="nuxt-link">аксессуары</NuxtLink>
+                <NuxtLink class="nuxt-link">контакты</NuxtLink>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+header {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: white;
+
+    .container {
+        max-width: 1206px;
+        flex-grow: 1;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0 22px;
+        padding: 22px 0;
+        border-bottom: 1px $primary solid;
+
+        .site-title {
+            padding: 5px;
+            font-size: 24px;
+            font-weight: 600;
+            color: $primary;
+        }
+
+        nav {
+            max-width: 435px;
+            flex-grow: 1;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            font-size: 18px;
+
+            .nuxt-link {
+                color: $font-color;
+            }
+        }
+    }
+}
+</style>
