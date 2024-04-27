b4c4456d27efcb995d57d821575f3113c9a63caa
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-27 15:59:20 +0300

    Fix deploy error 500: supabaseUrl is required

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import '@/assets/styles/gilroy-font.scss';
 import type { Database } from './database.types';
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://jbovtfmcskxgtxmagpsm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impib3Z0Zm1jc2t4Z3R4bWFncHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxNTIwNzUsImV4cCI6MjAyOTcyODA3NX0.KhnRq6v5LMGNSnPW2hIZsUXO6kqUimxhxS-GwgT45Jo')
 const client = useSupabaseClient<Database>();
 
 useHead({
