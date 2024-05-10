995e468e11cac03374f4950624733aac3ce39c84
dundduun <145420979+dundduun@users.noreply.github.com>
2024-05-10 22:38:39 +0300

    refactor: add landing-search folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import LandingSearch from '@/components/sections/LandingSearch.vue';
+import LandingSearch from '@/components/sections/landing-search/LandingSearch.vue';
 import GiftToken from '@/components/sections/GiftToken.vue';
 import MapPreview from '@/components/ui/footer/MapPreview.vue';
 import Contacts from '@/components/ui/footer/Contacts.vue';
