d9049f5597fa010823a04fd029b275df59d4b7a4
dundduun <145420979+dundduun@users.noreply.github.com>
2024-06-15 21:10:25 +0300

    refactor(meta): use appropriate composables (#41)

--- app.vue
+++ app.vue
@@ -6,15 +6,11 @@ useHead({
     titleTemplate: (titleChunk) => {
         return titleChunk? `Masters Bookstore | ${titleChunk}` : 'Masters Bookstore - книжный магазин';
     },
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    meta: [
-        { name: 'author', content: 'dundduun' },
-        {
-            name: 'description',
-            content:
-                'Книжный магазин редких и эксклюзивных изданий по истории искусства, архитектуре, дизайну, моде и фотографии.',
-        },
-    ],
+});
+
+useSeoMeta({
+    description: 'Книжный магазин редких и эксклюзивных изданий по истории искусства, архитектуре, дизайну, моде и фотографии.',
+    author: 'dundduun',
 });
 
 const { primary } = SCSSVariables;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,12 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            ],
+            htmlAttrs: {
+                lang: 'ru',
+            },
         },
     },
 
