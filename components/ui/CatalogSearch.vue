da6f8655e2a3bcde32bff15788acac481166317c
dundduun <145420979+dundduun@users.noreply.github.com>
2024-06-28 18:20:19 +0300

    feat(CatalogSearch): create search exterior view

---
+++ components/ui/CatalogSearch.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="catalog-search">
+        <input class="input" placeholder="Поиск" type="text" />
+        <button class="search-button">
+            <img
+                src="@/assets/images/loupe.svg"
+                alt="Искать"
+                class="loupe-icon"
+            />
+        </button>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.catalog-search {
+    width: 230px;
+    height: 30px;
+    display: flex;
+
+    .input {
+        padding: 0 0 0 10px;
+        border: 1px solid $catalog-search-border;
+        border-right: none;
+        border-radius: 3px 0 0 3px;
+        font-size: 16px;
+        font-family: $font-family;
+        font-weight: 300;
+        background-color: $catalog-search;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    .search-button {
+        display: flex;
+        align-items: center;
+        background-color: $catalog-search;
+        border: 1px solid $catalog-search-border;
+        border-radius: 0 3px 3px 0;
+
+        .loupe-icon {
+            height: 20px;
+            width: 20px;
+            filter: invert(0%) sepia(0%) saturate(0%) hue-rotate(304deg) brightness(97%) contrast(103%);
+        }
+    }
+}
+</style>
