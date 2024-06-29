5375f6cdaff50ef625622fa6efbdaff99f54a9af
dundduun <145420979+dundduun@users.noreply.github.com>
2024-06-29 19:50:06 +0300

    chore(searchFilter): add non-styled filter

--- components/ui/CatalogFilter.vue
+++ components/ui/CatalogFilter.vue
@@ -1,49 +1,74 @@
 <template>
     <div class="catalog-filter">
-        <input class="search-input" placeholder="Поиск" type="text" />
-        <button class="search-button">
-            <img
-                src="@/assets/images/loupe.svg"
-                alt="Искать"
-                class="loupe-icon"
-            />
-        </button>
+        <div class="search-input-button-container">
+            <input class="search-input" placeholder="Поиск" type="text" />
+            <button class="search-button">
+                <img
+                    src="@/assets/images/loupe.svg"
+                    alt="Искать"
+                    class="loupe-icon"
+                />
+            </button>
+        </div>
+
+        <select>
+            <option>По возрастанию</option>
+            <option>По убыванию</option>
+        </select>
     </div>
 </template>
 
 <style scoped lang="scss">
 .catalog-filter {
-    width: 230px;
     height: 30px;
     display: flex;
+    gap: 20px;
 
-    .search-input {
-        padding: 0 0 0 10px;
-        border: 1px solid $catalog-filter-border;
-        border-right: none;
-        border-radius: 3px 0 0 3px;
-        font-size: 16px;
-        font-family: $font-family;
-        font-weight: 300;
-        background-color: $catalog-filter;
-
-        &:focus {
-            outline: none;
-        }
-    }
-
-    .search-button {
+    .search-input-button-container {
+        height: 100%;
         display: flex;
-        align-items: center;
-        background-color: $catalog-filter;
-        border: 1px solid $catalog-filter-border;
-        border-radius: 0 3px 3px 0;
 
-        .loupe-icon {
-            height: 20px;
-            width: 20px;
-            filter: invert(0%) sepia(0%) saturate(0%) hue-rotate(304deg) brightness(97%) contrast(103%);
+        .search-input {
+            width: 230px;
+            padding-left: 10px;
+            border: 1px solid $catalog-filter-border;
+            border-right: none;
+            border-radius: 3px 0 0 3px;
+            font-size: 16px;
+            font-family: $font-family;
+            font-weight: 300;
+            background-color: $catalog-filter;
+
+            &:focus {
+                outline: none;
+            }
+        }
+
+        .search-button {
+            display: flex;
+            align-items: center;
+            background-color: $catalog-filter;
+            border: 1px solid $catalog-filter-border;
+            border-radius: 0 3px 3px 0;
+            cursor: pointer;
+            transition: 0.2s;
+
+            &:hover {
+
+                .loupe-icon {
+                    filter: invert(0%) sepia(0%) saturate(0%) hue-rotate(174deg) brightness(95%) contrast(102%);;
+                }
+            }
+
+            .loupe-icon {
+                height: 20px;
+                width: 20px;
+                filter: invert(48%) sepia(0%) saturate(0%) hue-rotate(220deg) brightness(94%) contrast(93%);
+                transition: 0.2s;
+            }
         }
     }
 }
 </style>
+<script setup lang="ts">
+</script>
