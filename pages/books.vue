f954116dd08e272e099eceeb2983695de7971390
dundduun <145420979+dundduun@users.noreply.github.com>
2024-07-05 19:44:13 +0300

    feat(books): fetch products data & add list

--- pages/books.vue
+++ pages/books.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import CategoriesSidebar from '@/components/pages/books/CategoriesSidebar.vue';
 import CatalogFilter from '@/components/ui/CatalogFilter.vue';
+import ProductsList from '@/components/pages/books/ProductsList.vue';
 import type { Database } from '@/database.types';
 
 const client = useSupabaseClient<Database>();
+
+const { data: products } = await client
+    .from('product')
+    .select('*')
+    .order('created_at', { ascending: false });
 </script>
 
 <template>
     <div class="books-page">
         <CategoriesSidebar />
 
-        <div class="filter-and-goods">
+        <div class="filter-and-products-list">
             <CatalogFilter />
+            <ProductsList :products />
         </div>
     </div>
 </template>
