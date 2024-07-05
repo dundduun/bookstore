b852fd5058067f4b8b9c6e3564a57afcf511d61b
dundduun <145420979+dundduun@users.noreply.github.com>
2024-07-05 19:46:18 +0300

    feat: add ProductList base

---
+++ components/pages/books/ProductsList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ProductCard from '@/components/pages/books/ProductCard.vue';
+import type { selectedProduct } from '@/types';
+
+defineProps<{
+    products: selectedProduct[] | null;
+}>();
+</script>
+
+<template>
+    <div class="products-list">
+        <ProductCard
+            v-for="(product, index) in products"
+            :product="product"
+            :key="index"
+        />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.products-list {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+</style>
