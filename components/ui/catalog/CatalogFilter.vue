e124a925258494494ac4d08ee07a7d3eb5d3e9ad
dundduun <sadfgqr@gmail.com>
2024-08-09 12:59:56 +0300

    refactor: decompose MobileFilters

--- components/ui/catalog/CatalogFilter.vue
+++ components/ui/catalog/CatalogFilter.vue
@@ -8,6 +8,7 @@ import {
     SelectValue,
 } from '@/components/ui/select';
 import CatalogSearch from '@/components/ui/catalog/CatalogSearch.vue';
+import MobileFilters from '@/components/ui/catalog/MobileFilters.vue';
 
 const selectItems = [
     { value: 'new-first', text: 'дата: сперва новые' },
@@ -23,17 +24,6 @@ const model = ref();
 onMounted(() => {
     model.value = 'new-first';
 });
-
-const activeFilter = ref('');
-function activateFilter(clickedFilter: string) {
-    if (!activeFilter.value) {
-        activeFilter.value = clickedFilter;
-    } else if (activeFilter.value !== clickedFilter) {
-        activeFilter.value = clickedFilter;
-    } else {
-        activeFilter.value = '';
-    }
-}
 </script>
 
 <template>
@@ -58,54 +48,7 @@ function activateFilter(clickedFilter: string) {
             </SelectContent>
         </Select>
 
-        <div class="mob-filters">
-            <div class="buttons">
-                <button
-                    class="filters"
-                    :class="{
-                        'active-filter': activeFilter === 'filtersButton',
-                    }"
-                    @click="activateFilter('filtersButton')"
-                >
-                    <img
-                        src="@/assets/images/settings.svg"
-                        class="settings-icon"
-                    />
-                    <span>фильтры</span>
-                </button>
-
-                <button
-                    class="search"
-                    :class="{
-                        'active-filter': activeFilter === 'searchButton',
-                    }"
-                    @click="activateFilter('searchButton')"
-                >
-                    <img src="@/assets/images/loupe.svg" class="loupe-icon" />
-                </button>
-            </div>
-
-            <div class="fickle-filters">
-                <CatalogSearch v-show="activeFilter === 'searchButton'" class="m-catalog-search" />
-
-                <div v-show="activeFilter === 'filtersButton'" class="sort-price-availability">
-                    <button class="sort filter">
-                        <span>сортировка</span>
-                        <img class="chevron-icon" src="@/assets/images/left-chevron.png" />
-                    </button>
-
-                    <button class="price filter">
-                        <span>цена</span>
-                        <img class="chevron-icon" src="@/assets/images/left-chevron.png" />
-                    </button>
-
-                    <button class="availability filter">
-                       <span>наличие</span>
-                       <img class="chevron-icon" src="@/assets/images/left-chevron.png" />
-                    </button>
-                </div>
-            </div>
-        </div>
+        <MobileFilters />
     </div>
 </template>
 
@@ -144,86 +87,6 @@ function activateFilter(clickedFilter: string) {
 
         @media (max-width: 980px) {
             display: none;
-        }
-    }
-
-    .mob-filters {
-        display: none;
-
-        @media (max-width: 980px) {
-            display: block;
-        }
-
-        .buttons {
-            display: flex;
-            justify-content: space-between;
-
-            .filters {
-                display: flex;
-                align-items: center;
-                gap: 10px;
-                transition: 0.2s;
-
-                .settings-icon {
-                    height: 25px;
-                }
-            }
-
-            .search {
-                padding: 10px 0;
-                transition: 0.2s;
-
-                .loupe-icon {
-                    width: 25px;
-                    height: 25px;
-                    filter: invert(0%) sepia(0%) saturate(0%) hue-rotate(174deg)
-                        brightness(95%) contrast(102%);
-                }
-            }
-
-            .active-filter {
-                filter: invert(60%) sepia(0%) saturate(55%) hue-rotate(157deg)
-                    brightness(106%) contrast(92%);
-            }
-        }
-
-        .fickle-filters {
-            width: 100%;
-            display: flex;
-            justify-content: center;
-
-            .m-catalog-search {
-                width: 100%;
-                height: 40px;
-                margin-top: 20px;
-            }
-
-            .sort-price-availability {
-                width: 100%;
-                display: flex;
-                flex-direction: column;
-                margin-top: 20px;
-                border: 1px solid $m-filters-border;
-                border-width: 1px 0px;
-
-                .filter {
-                    height: 40px;
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                    font-size: 15px;
-
-                    &.price {
-                        border: 1px solid $m-filters-border;
-                        border-width: 1px 0;
-                    }
-
-                    .chevron-icon {
-                        height: 15px;
-                        transform: rotate(180deg);
-                    }
-                }
-            }
         }
     }
 }
