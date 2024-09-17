21abd89d6e9690e345fb7b5b9430864639980136
dundduun <145420979+dundduun@users.noreply.github.com>
2024-09-17 21:06:07 +0300

    feat(filters.ts): add three state props

---
+++ stores/filters.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+
+export const useFiltersStore = defineStore(
+    'filters',
+    () => {
+        return { 
+            showInStockOnly: true,
+            sort: 'new-first', // isn't used
+            minPrice: 0,
+            maxPrice: 1_000_000, 
+        };
+    },
+    {
+        persist: true,
+    },
+);
