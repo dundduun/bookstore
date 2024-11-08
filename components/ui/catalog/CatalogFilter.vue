bfd6f080699e10faecb8b0a56e1f004a7d66be2f
dundduun <145420979+dundduun@users.noreply.github.com>
2024-11-08 11:04:07 +0300

    fix: remove shadcn-select usages

--- components/ui/catalog/CatalogFilter.vue
+++ components/ui/catalog/CatalogFilter.vue
@@ -1,52 +1,31 @@
 <script setup lang="ts">
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select';
 import CatalogSearch from '@/components/ui/catalog/CatalogSearch.vue';
 import MobFilters from '@/components/ui/catalog/MobFilters.vue';
 
 const selectItems = [
-    { value: 'new-first', text: 'дата: сперва новые' },
-    { value: 'old-first', text: 'дата: сперва старые' },
-    { value: 'alphabetical-order', text: 'название: А-Я' },
-    { value: 'alphabetical-reversed-order', text: 'название: Я-А' },
-    { value: 'descending', text: 'цена: по убыванию' },
-    { value: 'ascending', text: 'цена: по возрастанию' },
+    { value: 'by-date-new-first', text: 'дата: сперва новые' },
+    { value: 'by-date-old-first', text: 'дата: сперва старые' },
+    { value: 'by-name-alphabetical', text: 'название: А-Я' },
+    { value: 'by-name-alphabetical-reversed', text: 'название: Я-А' },
+    { value: 'by-price-descending', text: 'цена: по убыванию' },
+    { value: 'by-price-ascending', text: 'цена: по возрастанию' },
 ];
-
-const model = ref();
-
-onMounted(() => {
-    model.value = 'new-first';
-});
 </script>
 
 <template>
     <div class="catalog-filter">
         <CatalogSearch class="catalog-search" />
 
-        <Select class="select-filter" v-model="model">
-            <SelectTrigger class="select-filter">
-                <SelectValue :placeholder="selectItems[0].text" />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
-                    <SelectItem
-                        class="select-item"
-                        v-for="(item, index) in selectItems"
-                        :value="item.value"
-                        :key="index"
-                    >
-                        {{ item.text }}
-                    </SelectItem>
-                </SelectGroup>
-            </SelectContent>
-        </Select>
+        <div
+            class="select-filter"
+            tabindex="0"
+        >
+            <img
+                class="opening-chevron"
+                src="@/assets/images/left-chevron.png"
+                alt="expand"
+            />
+        </div>
 
         <MobFilters />
     </div>
@@ -75,18 +54,32 @@ onMounted(() => {
     }
 
     .select-filter {
-        width: 260px;
-        height: 30px;
-        padding-left: 10px;
+        display: flex;
+        justify-content: end;
+        align-items: center;
         border: 1px solid $catalog-filter-border;
         border-radius: 3px;
+        width: 260px;
+        height: 30px;
+        padding: 0 10px;
+        cursor: pointer;
         font-size: 16px;
         font-family: $font-family;
         font-weight: 300;
         background-color: $catalog-filter;
 
+        //&:focus {
+        //   background-color: green;
+        //}
+
         @media (max-width: 980px) {
             display: none;
+        }
+
+        .opening-chevron {
+            transform: rotate(270deg);
+            width: 16px;
+            height: 16px;
         }
     }
 }
